f1281bf64803d6cbb30eb26e5c6bee0fb29bfb00
trig001 <trig001@gmail.com>
2025-07-11 07:36:47 -0500

    new file:   index.html 	new file:   name.js

---
+++ name.js
@@ -0,0 +1,56 @@
+let humanScore = 0
+let computerScore = 0
+
+
+
+function getComputerChoice() {
+    const options = ["paper", "rock", "scissors"]
+    const randomValue = options[Math.floor(Math.random() * options.length)];
+    return(randomValue);
+}
+
+function getHumanChoice() {
+    const selected = ["paper", "rock", "scissors"];
+    let input;
+    input = prompt("Please pick Paper/Rock/Scissor").toLowerCase();
+
+    if (selected.includes(input)) {
+      return(input)
+    } else {
+    alert("Invalid Choice. Please enter paper, rock, scissors.")
+    }
+}
+
+function playRound(getHumanChoice, getComputerChoice){
+   
+    if (getHumanChoice === getComputerChoice) {
+        console.log("It's a tie, try again")
+    } else if ((getHumanChoice === "rock" && getComputerChoice === "scissors") ||
+        (getHumanChoice === "scissors" && getComputerChoice === "paper") ||
+        (getHumanChoice === "paper" && getComputerChoice === "rock")) {
+        console.log("You Win this round, but you beat a basic computer");
+        humanScore++;
+    } else {
+        console.log("You Lose this round, its sad because its a basic computer");
+        computerScore++;}
+
+    console.log(`Score - You: ${humanScore}, Computer: ${computerScore}`)
+}
+
+function playGame() {
+    while (humanScore < 3 && computerScore < 3){
+    const humanChoice = getHumanChoice();
+        if (!humanChoice) {
+            continue;
+        }
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice)
+    }
+
+    if (humanScore > computerScore) {
+        console.log("Congrats you won the Game")
+    } else {
+        console.log("Sorry you Lost to a computer")
+    }
+}
+
